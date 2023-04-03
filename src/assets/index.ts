e53305fe652fc00a1b5dc953f1be0800aff1be39
caarlosdamian <dapecarlos@gmail.com>
2023-04-02 18:36:56 -0600

    feat(assets): added assets imports

---
+++ src/assets/index.ts
@@ -0,0 +1,21 @@
+import iconClose from './icon-close.svg';
+import iconDarkMode from './icon-dark-mode.svg';
+import iconDelete from './icon-delete.svg';
+import iconDocument from './icon-document.svg';
+import iconHidePreview from './icon-hide-preview.svg';
+import iconLightMode from './icon-light-mode.svg';
+import iconMenu from './icon-menu.svg';
+import iconSave from './icon-save.svg';
+import iconShowPreview from './icon-show-preview.svg';
+
+export {
+  iconClose,
+  iconDarkMode,
+  iconDelete,
+  iconDocument,
+  iconHidePreview,
+  iconLightMode,
+  iconMenu,
+  iconSave,
+  iconShowPreview,
+};
