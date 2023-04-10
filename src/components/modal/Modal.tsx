86d122bd1696704d3d8041a65544ff777b4b0d76
caarlosdamian <dapecarlos@gmail.com>
2023-04-09 22:45:45 -0600

    feat(darkmode): added darkmode

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -1,23 +1,24 @@
 import React from 'react';
-import './Modal.scss';
 import { Button } from '../button/Button';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '../../redux/store';
 import { deleteFile } from '../../redux/slices/filesSlice';
+import './Modal.scss';
+
 export const Modal = ({
   setShow,
 }: {
   setShow: React.Dispatch<React.SetStateAction<boolean>>;
 }) => {
   const {
-    files: { selectedFile },
+    files: { selectedFile },theme:{dark}
   } = useSelector((state: RootState) => state);
   const dispatch = useDispatch();
   return (
-    <div className="modal__overlay" onClick={() => setShow(false)}>
-      <div className="modal__container">
-        <span className="modal__container--title">Delete this document?</span>
-        <span className="modal__container--description">
+    <div className={`modal__overlay ${dark && 'dark'}`} onClick={() => setShow(false)}>
+      <div className={`modal__container ${dark && 'dark'}`}>
+        <span className={`modal__container--title ${dark && 'dark'}`}>Delete this document?</span>
+        <span className={`modal__container--description ${dark && 'dark'}`}>
           Are you sure you want to delete the '{selectedFile.name}' document and
           its contents? This action cannot be reversed.
         </span>
